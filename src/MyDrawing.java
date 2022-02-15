276a4735d895cd2affd15c8b19b057d09a3db660
Nathan Bush <nbush@mines.edu>
2022-02-15 14:54:18 -0700

    moved first cat and added another cat

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -14,7 +14,9 @@ public class MyDrawing extends JPanel {
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
-		cat.draw(g, 50, 50);
+		cat.draw(g, 150, 150);
+		cat.draw(g, 10, 10);
+
 	}
 	
 	/**
